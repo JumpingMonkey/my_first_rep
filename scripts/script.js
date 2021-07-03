0bfcce0adeac6b565f5bee074e98a0bc7228f307
AlexMorozov <morozovprav@gmail.com>
2021-07-03 19:08:41 +0300

    add pop up

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,11 @@ btn.addEventListener("click", function(){
     console.log(btn.classList.contains('btn'));
 });
 
+
+var slider = document.querySelector('.range-line')
+            var output = document.querySelector('#range-value')
+            output.innerHTML = slider.value
+            slider.oninput = function () {
+                output.innerHTML = this.value
+            }
+
